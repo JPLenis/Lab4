753e61b392a33ac4e8e4c2b09651d9f415816540
JPLenis <jlenis@my.centennialcollege.ca>
2025-04-06 11:00:04 -0400

    Add project files.

---
+++ Lab4/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab4
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return false;
+
+            return char.IsUpper(str[0]);
+        }
+    }
+}
